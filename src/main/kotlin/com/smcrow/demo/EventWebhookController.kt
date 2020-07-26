320abb39a2b74c8a69147e853971be1713a3e17e
Steve Crow <steve@smcrow.com>
2020-07-26 18:26:11 -0400

    Produce Kafka messages from webhook controller.

--- src/main/kotlin/com/smcrow/demo/EventWebhookController.kt
+++ src/main/kotlin/com/smcrow/demo/EventWebhookController.kt
@@ -1,16 +1,17 @@
 package com.smcrow.demo
 
 import org.slf4j.LoggerFactory
+import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class EventWebhookController {
+class EventWebhookController(private val kafkaTemplate: KafkaTemplate<String, String>) {
     @PostMapping("/webhooks/events")
     fun incomingEvents(@RequestBody body: String) {
         // Handle the Event Here
-        log.info(body)
+        kafkaTemplate.send("vonage.webhook.events", body)
     }
 
     companion object {
